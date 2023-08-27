d67ec7ebf49d5bd97d85c0485f360e04b4b85ba4
Ritwick Das <ritwick35rd@live.com>
2023-08-27 13:36:44 +0530

    - added error handling - added exception handling to roll back transaction in case of failure

--- controllers/transactions.controller.js
+++ controllers/transactions.controller.js
@@ -9,7 +9,7 @@ const Decimal = require('decimal.js');
 const initialiseWallet = (req, res) => {
     const { balance, name } = req.body;
 
-    const amountString = balance.toString(10);
+    const amountString = balance.toString(10).trim();
 
     if (verifyAmount(amountString)) {
         const wallet = new Wallet({
@@ -17,28 +17,46 @@ const initialiseWallet = (req, res) => {
             name,
             date: Date.now()
         })
-
+        console.log("saving wallet with initial 0 balance")
         wallet.save().then(wallet => {
+            console.log("wallet saved")
+            try {
+                console.log("saving the initial wallet transaction")
+                const transaction = new Transaction({
+                    walletId: wallet._id,
+                    amount: amountString,
+                    balance: amountString,
+                    description: 'Initial Wallet Load',
+                    date: Date.now(),
+                })
 
-            const transaction = new Transaction({
-                walletId: wallet._id,
-                amount: amountString,
-                balance: amountString,
-                description: 'Initial Wallet Load',
-                date: Date.now(),
-            })
-
-            transaction.save().then(transaction => {
-                wallet.balance = transaction.balance;
-                wallet.save().then((wallet) => {
-                    return void res.status(HttpStatusCode.Ok).send({
-                        id: wallet._id,
-                        balance: wallet.balance,
-                        name: wallet.name,
-                        date: wallet.date
+                transaction.save().then(transaction => {
+                    console.log("transaction saved")
+                    wallet.balance = transaction.balance;
+                    wallet.save().then((wallet) => {
+                        console.log("wallet balance updated according to transaction amount")
+                        if (wallet.balance !== transaction.balance) {
+                            Transaction.deleteOne({ _id: transaction._id }).then(() => {
+                                throw new Error("Transaction failed. Balance mismatch. Rolling back")
+                            });
+                        }
+                        return void res.status(HttpStatusCode.Ok).send({
+                            id: wallet._id,
+                            balance: wallet.balance,
+                            name: wallet.name,
+                            date: wallet.date
+                        })
                     })
                 })
-            })
+            }
+            catch (e) {
+                // saving old balance to the wallet in case it was overwrittens
+                Wallet.deleteOne({ _id: wallet._id }).then(() => {
+                    return void res.status(HttpStatusCode.InternalServerError).send({ message: "Transaction unsuccessful. Could not create wallet. Please try again", e })
+                })
+            }
+
+
         })
     }
     else
@@ -73,19 +91,25 @@ const transact = async (req, res) => {
     const walletId = req.params.walletId
     const { amount, description } = req.body
 
-    const amountString = amount.toString(10);
+    const amountString = amount.toString(10).trim();
 
     if (verifyAmount(amountString)) {
 
         const wallet = await Wallet.findOne({ "_id": new ObjectId(walletId) })
 
         if (wallet) {
+            console.log("Found wallet. Initiating transaction")
             const originalWalletBalance = wallet.balance;
 
             const currentBal = new Decimal(originalWalletBalance);
             const transsactionAmt = new Decimal(amountString)
             try {
-                const newBal = currentBal.plus(transsactionAmt)
+                const newBal = currentBal.plus(transsactionAmt)  // calculating new balance
+                if (Number(newBal) < 0) {
+                    return void res.status(HttpStatusCode.NotAcceptable).send({ "message": "Insufficient Balance for " + wallet.name })
+                }
+
+                console.log("Balance check complete. Transaction can continue")
 
                 const transaction = new Transaction({
                     walletId: wallet._id,
@@ -96,8 +120,17 @@ const transact = async (req, res) => {
                 })
 
                 transaction.save().then(transaction => {
+                    console.log("Transaction details saved")
                     wallet.balance = transaction.balance;
                     wallet.save().then((wallet) => {
+
+                        if (wallet.balance !== transaction.balance) {
+                            Transaction.deleteOne({ _id: transaction._id }).then(() => {
+                                throw new Error("Transaction failed. Balance mismatch. Rolling back")
+                            });
+                        }
+                        console.log("Wallet Balance Updated")
+
                         return void res.status(HttpStatusCode.Ok).send({
                             balance: wallet.balance,
                             transactionId: transaction._id
